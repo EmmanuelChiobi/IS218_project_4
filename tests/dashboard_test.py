eb54fccb980cc00d37099ad93909e2ee74c9d794
Emmanuel Chiobi <ec362@njit.edu>
2022-05-12 08:39:50 -0400

    Edited dashboard test

--- tests/dashboard_test.py
+++ tests/dashboard_test.py
@@ -1,1 +1,25 @@
-"""This tests the dashboard page"""
+"""This tests the access to the dashboard page"""
+
+from flask_login import FlaskLoginClient
+from app import db
+from app.db.models import User
+
+def non_admin_test(application):
+    """This test the non-admin users' access to the dashboard page"""
+    application.test_client_class = FlaskLoginClient
+    with application.test_client(user = None) as client:
+        response = client.get('/dashboard')
+        assert response.status_code == 302
+
+def admin_test(application):
+    """This tests the admin users' access to the dashboard page"""
+    application.test_client_class = FlaskLoginClient
+    user = User('bobgale@gmail.com', 'newtest', True)
+    db.session.add(user)
+    db.session.commit()
+    assert user.email == 'bobgale@gmail.com'
+    with application.test_client(user = user) as client:
+        response = client.get('/dashboard')
+        assert b'/bobgale@gmail.com' in response.data
+        assert response.status_code == 302
+        
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,10 +4,10 @@ def test_request_main_menu_links(client):
     """This tests the main menu links in HTML files"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'/href="about"' in response.data
-    assert b'/href="welcome"' in response.data
-    assert b'/href="login"' in response.data
-    assert b'/href="register"' in response.data
+    assert b'/href="About"' in response.data
+    assert b'/href="Welcome"' in response.data
+    assert b'/href="Login"' in response.data
+    assert b'/href="Register"' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
